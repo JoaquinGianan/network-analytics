5ea454983fc99f1a8cdfe598466c01b7c852366f
JoaquinGianan <90005176+JoaquinGianan@users.noreply.github.com>
2022-12-01 20:12:02 -0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   notebooks/tpot_network_analytics.py
    	new file:   notebooks/tpot_network_analytics_2.py
    	new file:   powerpoint/Network Analysis - MLE Capstone Presentation_v06.pptx

--- notebooks/tpot_network_analytics_2.py
+++ notebooks/tpot_network_analytics_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.9521518326659425
-exported_pipeline = XGBClassifier(learning_rate=1.0, max_depth=5, min_child_weight=8, n_estimators=100, n_jobs=1, subsample=0.8500000000000001, verbosity=0)
+# Average CV score on the training set was: 0.9093493283269428
+exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.3, min_samples_leaf=8, min_samples_split=4, n_estimators=100)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
